717dc1ebc6c2a6e6bfdeb7bccdf559a9b96c412a
Felipe <fsa4@aluno.ifal.edu.br>
2022-06-08 13:34:14 -0300

    Mudanças

--- Capitulo2/script.js
+++ Capitulo2/script.js
@@ -2,6 +2,9 @@
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 
+const alternarBtn = document.getElementById("alternarBtn");
+const cores = ["black", "gray", "red", "yellow", "green", "blue", "purple"];
+
 const b1 = new Bola(context);
 b1.x = 100;
 b1.y = 200;
@@ -22,3 +25,36 @@ const animacao = new Animacao(context);
 animacao.novoSprite(b1);
 animacao.novoSprite(b2);
 animacao.ligar();
+
+alternarBtn.addEventListener("click", function () {
+  if (animacao.ligado) {
+    animacao.desligar();
+    context.fillStyle = "rgba(0, 0, 0, 0.3)";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+  } else {
+    animacao.ligar();
+  }
+});
+
+canvas.addEventListener("mousedown", function (e) {
+  const bola = new Bola(context);
+  bola.x = e.offsetX;
+  bola.y = e.offsetY;
+  bola.velocidadeX = getRandomIntegerSign(1, 20);
+  bola.velocidadeY = getRandomIntegerSign(1, 20);
+  bola.cor = cores[getRandomInteger(0, cores.length)];
+  bola.raio = getRandomInteger(15, 30);
+  animacao.novoSprite(bola);
+});
+
+function getRandomBoolean() {
+  return Math.random() >= 0.5;
+}
+
+function getRandomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getRandomIntegerSign(min, max) {
+  return getRandomInteger(min, max) * (getRandomBoolean() ? 1 : - 1);
+}
