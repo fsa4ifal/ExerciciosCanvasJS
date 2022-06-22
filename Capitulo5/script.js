3bd278862b68cd5f0b79c9fcd51b2c24f4ec9a0b
Felipe <fsa4@aluno.ifal.edu.br>
2022-06-22 13:28:04 -0300

    Ajuste

--- Capitulo5/script.js
+++ Capitulo5/script.js
@@ -8,7 +8,7 @@ b1.y = 60;
 b1.velocidadeX = 10;
 b1.velocidadeY = -5;
 b1.cor = "blue";
-b1.raio = 20;
+b1.raio = 30;
 
 const b2 = new Bola();
 b2.x = 60;
@@ -16,7 +16,7 @@ b2.y = canvas.height - 60;
 b2.velocidadeX = -10;
 b2.velocidadeY = 10;
 b2.cor = "red";
-b2.raio = 30;
+b2.raio = 40;
 
 const colisor = new Colisor();
 colisor.novoSprite(b1);
